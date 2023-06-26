701605fd81fe3d90b70e677ccf5e457637854829
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-06-26 20:51:39 +0100

    task7

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,16 +1,10 @@
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = {
-      income: income,
-      gdp: gdp,
-      capita: capita,
-    };
-  
-    return {
-        'income': budget.income,
-        'gdp': budget.gdp,
-        'capita': budget.capita
-    };
-  }
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
 
+  return budget;
+}
 
-  console.log(getBudgetObject(400, 700, 900));
