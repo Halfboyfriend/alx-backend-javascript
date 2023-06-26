b642cc30f56ebaf1697bdf2fc8accd87064752e0
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-06-26 19:54:35 +0100

    ES^ basics

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+function taskBlock(trueOrFalse) {
+    var task = false;
+    var task2 = true;
+  
+    if (trueOrFalse) {
+       task = true;
+       task2 = false;
+    }
+  
+    return [task, task2];
+  }
+
+console.log(taskBlock(true));
+console.log(taskBlock(false));
