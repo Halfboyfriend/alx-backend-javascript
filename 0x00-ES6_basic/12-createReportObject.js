b705f28d1e37d54b2021c3265e6168d4b1e51517
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-06-27 13:19:38 +0100

    task1 enslint

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,17 +1,6 @@
-const createEmployeesObject = require('./11-createEmployeesObject');
-
-function createReportObject(employeesList) {
+export default function createReportObject(employeesList) {
     const employList = {
         [employeesList]: {},
     }
     return employList;
 }
-
-const employ = {
-    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
-    ...createReportObject('marketing', ['Sylvie']),
-}
-
-const report = createReportObject(employ);
-console.log(report.allEmployees);
-console.log(report.getNumberOfDepartments(report.allEmployees));
