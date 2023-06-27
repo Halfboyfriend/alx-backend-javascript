8f9dcff499acb0ec6d34c65896d793679d208ada
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-06-27 15:22:01 +0100

    promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,17 +2,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup (firstName, lastName, filename) {
-    const user = await signUpUser(firstName, lastName).then((data) => ({
-        status: 'fulfilled',
-        value: data,
-      }));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userData = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
 
-      const file = await uploadPhoto(filename).catch((error) => ({
-        status: 'rejected',
-        value: error.toString(),
-      }));
+  const fileData = await uploadPhoto(fileName).catch((err) => ({
+    status: 'rejected',
+    value: err.toString(),
+  }));
 
-      return Promise.resolve([user, file]);
-
+  return Promise.resolve([userData, fileData]);
 }
