28fb99764c44ed11db99722ec9516bc9ee3a8940
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-06-27 13:42:42 +0100

    task13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,6 @@
 export default function createIteratorObject(report) {
   const empty = [];
-  for (const k of object.keys(report.allEmployees)) {
+  for (const k of Object.keys(report.allEmployees)) {
     for (const j of report.allEmployees[k]) {
       empty.push(j);
     }
