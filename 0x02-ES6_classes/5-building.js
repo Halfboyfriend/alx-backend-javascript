99fad9e5213ff2aacaa923d4b0ea537d6f5d05e0
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-06-30 17:48:56 +0100

    autoCheckingSecondReview

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,5 @@
 /*eslint-disable */
-class Building {
+export default class Building {
     constructor (sqft){
         if (this.constructor !== Building && typeof(this.evacuationWarningMessage) !== 'function') {
             throw new Error('Class extending Building must override evacuationWarningMessage');
