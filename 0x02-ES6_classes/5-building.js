dc5137989184abf806e11993fdaef7ba5856a775
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-06-30 17:50:17 +0100

    autoCheckingSecondReview

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -12,15 +12,3 @@ export default class Building {
     }
 
 }
-
-const b = new Building(100);
-console.log(b);
-
-class TestBuilding extends Building {}
-
-try {
-    new TestBuilding(200)
-}
-catch(err) {
-    console.log(err);
-}
