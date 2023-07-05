73d8c75c510574fe6fa819132fec3e855976a72e
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-05 11:00:11 +0100

    task8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 /*eslint-disable */
-function cleanSet(set, startString) {
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }  
   const newString = Array.from(set)
     .filter((val) => val.startsWith(startString))
     .map((val) => val.slice(startString.length));
@@ -7,5 +10,3 @@ function cleanSet(set, startString) {
   return newString.join('-');
 }
 
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
