dbb412b2e6bde3b755b27b66c228b63be575e120
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-05 23:00:20 +0100

    task3

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -7,3 +7,12 @@ interface MinorCredits {
   credits: number;
   brand: 'minor';
 }
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
+  return subject1.credits + subject2.credits
+}
+
+function sumMinorCredits(subject1:MinorCredits, subject2:MinorCredits) {
+  return subject1.credits + subject2.credits;
+
+}
