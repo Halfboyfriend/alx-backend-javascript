920ededeed1e37567be4c8fc20f356f27c0fe307
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-05 10:20:13 +0100

    task5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+/*eslint-disable */
+function createInt8TypedArray (length, position, value) {
+    if (position < 0 || position >= length){
+        throw new Error('Position outside range');
+    }
+
+    const buffer = new ArrayBuffer(length);
+    const showCase = new Int8Array(buffer);
+    showCase[position] = value;
+    return new DataView(buffer);
+}
