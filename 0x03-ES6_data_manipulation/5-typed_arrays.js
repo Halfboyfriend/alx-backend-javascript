d567c14669fca0cf77e188e629a3488a7ed8c86c
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-05 10:24:50 +0100

    task5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,10 @@
-/*eslint-disable */
-export default function createInt8TypedArray (length, position, value) {
-    if (position < 0 || position >= length){
-        throw new Error('Position outside range');
-    }
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
 
-    const buffer = new ArrayBuffer(length);
-    const showCase = new Int8Array(buffer);
-    showCase[position] = value;
-    return new DataView(buffer);
+  const buffer = new ArrayBuffer(length);
+  const showCase = new Int8Array(buffer);
+  showCase[position] = value;
+  return new DataView(buffer);
 }
