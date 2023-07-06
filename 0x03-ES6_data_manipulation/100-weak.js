21259e4ee99cd9ad6a8e2b50bd7fb65b20f13a38
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-06 01:48:58 +0100

    task100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,12 @@
 /*eslint-disable */
 export const weakMap = new WeakMap();
-
 let count = 1;
 
-export function queryApi(endPoint){
-    weakMap.set(endPoint, count);
-    count++;
-    const qCount = weakMap.get(endPoint);
-    if (qCount >= 5) {
-        throw new Error('Endpoint load is high.');
-    }
+export function queryAPI(endPoint) {
+  weakMap.set(endPoint, count);
+  count += 1;
+  const queryCount = weakMap.get(endPoint);
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
 }
