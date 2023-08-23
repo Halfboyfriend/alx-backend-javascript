210f820f5333b91d3ac63c0e59f491c6ba01d546
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-23 19:26:59 +0100

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,13 @@
-prompt('Welcome to Holberton School, what is your name? ');
+process.stdout.write('Welcome to Holberton School, what is your name? ');
+
+process.stdin.on('readable', () => {
+    const input = process.stdin.read();
+
+    if(input) {
+        process.stdout.write(`Your name is ${input}`)
+    }
+});
+
+process.stdin.on('end', () => {
+    process.stdout.write(`Thats the end. Thanks`)
+})
