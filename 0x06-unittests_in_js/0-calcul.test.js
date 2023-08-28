91d070fa7919fd7e9c094d1d85d50d849e68c42b
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-08-28 19:03:08 +0100

    testing

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -25,12 +25,4 @@ describe('Testing Calculations',() => {
       it('rounding up a and b\'s floating point fractional numbers', () => {
         assert.strictEqual(calculateNumber(2.6, 2.5), 6);
       });
-    
-      it('rounding up a\'s floating point fractional numbers', () => {
-        assert.strictEqual(calculateNumber(2.6, 2.0), 5);
-      });
-    
-      it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
-        assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
-      });
 })
